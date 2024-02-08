cad436006a6188338083f04d1bbde240c02f8683
RokasV <vezikauskas.r@gmail.com>
2024-02-08 10:09:32 +0200

    tuple

---
+++ src/tuple.ts
@@ -0,0 +1,9 @@
+//  Tuple
+
+type UserArr = [string, number, boolean];
+
+const user: [string, number, boolean] = ['James', 23, true];
+
+const user2: UserArr = ['Jane', 33, false];
+
+user[0] = 'Mike';
